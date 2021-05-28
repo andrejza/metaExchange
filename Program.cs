c001ffc9cd2472d5f6e4f6c33dedbc3b8f953618
Andrej Zadnik <andrej_zadnik@hotmail.com>
2021-05-28 10:53:22 +0200

    First commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace metaExchange
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
